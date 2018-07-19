63bad38933978326cc0d25bd90c88239134b7d76
agusDev <curieltapiaagustin0112@gmail.com>
2018-07-19 17:33:31 -0500

    avance

---
+++ app/router-config.js
@@ -0,0 +1,16 @@
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "./inicio.html"
+        })
+        .when("/lugares", {
+            templateUrl: "./lugares.html"
+        })
+        .when("/quienes", {
+            templateUrl: "./quienes.html"
+        })
+        .when("/contacto", {
+            templateUrl: "./contacto.html"
+        });
+
+});
